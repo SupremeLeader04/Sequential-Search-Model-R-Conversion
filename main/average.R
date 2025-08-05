294f9d1cf7013a161e2044372f371da24cc70f0d
Hayden Dyke <haydendyke1@gmail.com>
2025-08-05 20:33:10 +0100

    Updating average.R to compute standard deviations.

--- main/average.R
+++ main/average.R
@@ -1,8 +1,21 @@
 d = list.files(pattern = "^rezSimWeitz_ghk_D100S.*\\.csv$")
-cumdata = c(rep(0, 10))
-for (file in d) {
-  data = head(read.csv(file, header=TRUE),-2)
-  cumdata = cumdata + data[[1]]
+num_files = length(d)
+data_matrix = matrix(0, nrow = 12, ncol = num_files)
+
+for (i in 1:num_files) {
+  file = d[i]
+  data = read.csv(file, header = TRUE)[1:12, 1]
+  data_matrix[, i] = data
 }
-avg = cumdata / 50
-write.table(avg, "overall_results.csv", sep = ",", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+M = numeric(10)  
+M2 = numeric(10) 
+
+for (i in 1:10) {
+  M[i] = mean(data_matrix[i, ])
+  print(data_matrix[i,])
+  M2[i] = sd(data_matrix[i, ])
+}
+
+write.table(M, "result_ghk_mean_Sum.csv", sep = ",", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(M2, "result_ghk_std_Sum.csv", sep = ",", row.names = FALSE, col.names = FALSE, quote = FALSE)
